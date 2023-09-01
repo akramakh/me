32b3a4590dcb573931c03e187a82d23ba5ec9292
akramakh <akram.icode@gmail.com>
2023-09-01 21:40:18 +0300

    add new website content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
